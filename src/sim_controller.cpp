425c12cd8d7b62af6dc081dbf90c50a41e0dd313
dhiegov <dhiegov@proton.me>
2025-09-29 02:06:55 -0300

    fix(sim_controller): Forgot adding namespace specifier

--- src/sim_controller.cpp
+++ src/sim_controller.cpp
@@ -4,20 +4,20 @@
 #include "sim_controller.h"
 #include "water_meter.h"
 
-SimController()
+SimController::SimController()
 {
     this->n_meters = 1;
     this->meters[0] = new WaterMeter();
 }
 
-~SimController()
+SimController::~SimController()
 {
     for (int i = 0; i < n_meters; i++) {
         delete this->meters + i;
     }
 }
 
-void startSim(void)
+void SimController::startSim(void)
 {
     this->meters[0].startMeter();
 }
