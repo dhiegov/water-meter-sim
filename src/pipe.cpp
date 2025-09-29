f144f76baf867e72afc81c84193002725811f989
dhiegov <dhiegov@proton.me>
2025-09-29 02:30:32 -0300

    (pipe) Forgot adding consistency checks

--- src/pipe.cpp
+++ src/pipe.cpp
@@ -1,13 +1,29 @@
 
+// std::max, std::min
+#include <algorithm>
+
 #include "pipe.h"
 
 Pipe::Pipe(float init_flow, float max_flow)
-: curr_flow_rate_lps(init_flow), max_flow_rate_lps(max_flow)
-{}
+{
+    init_flow = std::max(init_flow, 0.0f);
+    max_flow = std::max(max_flow, 0.0f);
+    if (init_flow >= max_flow) {
+        this->curr_flow_rate_lps = max_flow;
+        this->max_flow_rate_lps = max_flow;
+    } else {
+        this->curr_flow_rate_lps = init_flow;
+        this->max_flow_rate_lps = max_flow;
+    }
+}
 
 float Pipe::flowLps(void) const
 {
-    return this->curr_flow_rate_lps;
+    if (this->curr_flow_rate_lps >= this->maxFlowLps()) {
+        return this->maxFlowLps();
+    } else {
+        return this->curr_flow_rate_lps;
+    }
 }
 
 float Pipe::maxFlowLps(void) const
@@ -18,8 +34,8 @@ float Pipe::maxFlowLps(void) const
 void Pipe::setFlowLps(float f)
 {
     if (f <= 0.0f) {
-        this->curr_flow_rate_lps = 0;
+        this->curr_flow_rate_lps = 0.0f;
     } else {
-        this->curr_flow_rate_lps = f;
+        this->curr_flow_rate_lps = std::min(f, this->maxFlowLps());
     }
 }
