979dafe66496fdee86147dea569819d77c9ce122
dhiegov <dhiegov@proton.me>
2025-09-29 13:18:16 -0300

    (main) Run sim_controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,10 @@
 
 #include <iostream>
 
-#include "my_window.h"
+#include "sim_controller.h"
 
 int main()
 {
-    /* water meter image from
-     * Grupo Águas do Brasil; Como usar o seu hidrômetro; opened 2025-09-28
-     * https://www.grupoaguasdobrasil.com.br/wp-content/uploads/2018/04/como-usar-seu-hidrometro.pdf
-     */
-    MyWindow win("meter_bg.png");
-
-    while (! win.shouldClose())
-    {
-        win.drawMeter();
-        win.setDigitsPos(435, 290);
-        win.drawDigits("123456");
-        win.renderCanvas();
-    }
+    SimController sim;
+    sim.startSim();
 }
