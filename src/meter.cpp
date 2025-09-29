bbb71db57e986a5eae8e023136b12e2789d89902
dhiegov <dhiegov@proton.me>
2025-09-29 03:19:11 -0300

    (meter) Implement class, add more precision and methods

---
+++ src/meter.cpp
@@ -0,0 +1,38 @@
+
+// std::min, std::max
+#include <algorithm>
+
+#include "meter.h"
+#include "pipe.h"
+
+Meter::Meter(Pipe *in, Pipe *out)
+: in(in), out(out), count_liters(0), ms_passed(0)
+{}
+
+Meter::Meter(Pipe *in, Pipe *out, int init_count_m3)
+: in(in), out(out), ms_passed(0)
+{
+    this->count_liters = std::max(0, init_count_m3*1000);
+}
+
+/* Calculates how much water passed and increments counters */
+void Meter::measureFlow(Pipe *in, Pipe *out, int time_ms)
+{
+    this->ms_passed += time_ms;
+    double time_s = time_ms / 1000.0d;
+    double flow = std::min(this->in->flowLps(), this->out->flowLps());
+    this->count_liters += flow * time_s;
+}
+
+unsigned int Meter::countLiters(void) const
+{
+    return static_cast<unsigned int>(this->count_liters);
+}
+
+unsigned int Meter::count_m3(void) const
+{
+    return static_cast<unsigned int>(this->count_liters / 1000);
+}
+
+//unsigned int Meter::count_mm3(void) const;
+
--- src/meter.h
+++ src/meter.h
@@ -6,14 +6,16 @@
 class Meter
 {
 private:
-    float count_liters;
+    double count_liters;
     unsigned int ms_passed;
     Pipe *in;
     Pipe *out;
 public:
+    Meter(Pipe *in, Pipe *out);
     Meter(Pipe *in, Pipe *out, int init_count_m3);
     /* Calculates how much water passed and increments counters */
     void measureFlow(Pipe *in, Pipe *out, int time_ms);
+    unsigned int countLiters(void) const;
     unsigned int count_m3(void) const;
     unsigned int count_mm3(void) const;
 };
